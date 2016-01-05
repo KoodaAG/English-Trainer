efedd26fe4f5749bb9fd5fc90e89568b8ee596d3
KoodaAG <kofi.offei.ko@gmail.com>
2016-01-05 12:11:10 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,34 +6,22 @@
 #include <fstream>
 #include <sstream>
 #include <random>
+#include <assert.h>
 #include "Menu.h"
 
 using namespace std;
 
 vector<map<string, string>> loadUnit(unsigned un);		// Get Unit phrases from file
-
 void read();
 void write();
-
 void menuGetKey(Menu&);
 void unitGetKey(Menu&, vector<map<string, string>>& unit);
+//vector<map<string, string>> getRandomWords(vector<map<string, string>>& unit);
 
-vector<map<string, string>> getRandomWords(vector<map<string, string>>& unit)
-{
-	random_device rd;   // non-deterministic generator
-	mt19937 gen(rd());  // to seed mersenne twister.
-	uniform_int_distribution<> dist(0, unit.size()-1); // distribute results between 1 and 6 inclusive.
-
-	vector<map<string, string>> randomWord;
-
-	for (int i = 0; i < 10; ++i)
-		randomWord.push_back(unit.at(dist(gen)));
-
-	return randomWord;
-}
 
 int main()
 {
+	cout << "<--! NOTE WRITE THE VOCABULARIES IN THE UNIT TXT FILES !" << endl;
 	// Display Menu Options
 	Menu mainMenu;
 	mainMenu.addMenu("Read");
@@ -45,10 +33,30 @@ int main()
 	return 0;
 }
 
+
+
+//// Return vector with random vocabularies
+//vector<map<string, string>> getRandomWords(vector<map<string, string>>& unit)
+//{
+//	random_device rd;   // non-deterministic generator
+//	std::default_random_engine gen(rd());
+//	std::uniform_int_distribution<int> dist(0, 9);
+//
+//	vector<map<string, string>> randomWord;
+//
+//	for (int i = 0; i < 10; i++)
+//		randomWord.push_back(unit.at(dist(gen)));
+//
+//	return randomWord;
+//}
+//
+
+
+// User wants to read - remember vocabularies
 void read()
 {
 	vector<map<string, string>> unit;					// Contains the english and german vocabularies
-	vector <map<string, string>> randomWord;			// Contains 10 random vocabularies to learn
+	//vector<map<string, string>> randomWord;			// Contains 10 random vocabularies to learn
 
 	Menu unitList;
 	unitList.addMenu("Unit 1");
@@ -60,14 +68,19 @@ void read()
 	unitList.addMenu("Unit 7");
 	unitList.displayMenu();
 
-	menuGetKey(unitList);
-	randomWord = getRandomWords(unit);
+	unitGetKey(unitList, unit);
+	//randomWord = getRandomWords(unit);
 
-	for (int i = 0; i < 10; ++i)
-		cout << randomWord.at(i).begin()->first << " - " << randomWord.at(i).begin()->second << endl;
+	system("cls");
+	cout << "\t\tOutput Vocabularies!" << endl;
 
+	for (int i = 0; i < unit.size(); i++)
+		cout << unit.at(i).begin()->first << " - " << unit.at(i).begin()->second << endl;
+
+	system("pause");
 }
 
+// User wants to check his knowlege about writing vocabularies
 void write()
 {
 	vector<map<string, string>> unit;					// Contains the english and german vocabularies
@@ -82,34 +95,72 @@ void write()
 	unitList.addMenu("Unit 7");
 	unitList.displayMenu();
 
-	menuGetKey(unitList);
+	unitGetKey(unitList, unit);
+	system("cls");
+	
+	string enter;
+
+	for (int i = 0; i < unit.size(); i++) {
+		cout << "\t\tOutput Vocabularies!" << endl;
+		cout << "Enter the english definition" << endl << endl;
+		cout << unit.at(i).begin()->first << ": ";
+		cin >> enter;
+		cin.ignore();
+		cin.sync();
+		cin.clear();
+		if (enter != unit.at(i).begin()->second)
+		{
+			cout << "Wrong!";
+			Sleep(2000);
+			system("cls");
+		}
+		else
+		{
+			cout << "Right!";
+			Sleep(2000);
+			system("cls");
+		}
+	}
 
 }
 
+// Get the right file out of the main folder
 vector<map<string, string>> loadUnit(unsigned un)
 {
 	vector<map<string, string>> unit;
 
-	string path = "unit_"+ static_cast<char>('0'+un);
-	path.append(".txt");
+	string path = "Unit_";
+	path += static_cast<char>('0' + un);
+	path += ".txt";
 
-	ifstream file(path);
-	string line, firstword, secondword;
-	stringstream buf;
-	map<string, string> mapBuf;
+	cout << "OPEN: " << path << endl;
 
-	while (std::getline(file, line))
-	{
-		buf.str(line);
-		buf >> firstword >> secondword;
-		mapBuf.insert({ firstword, secondword });
-		unit.push_back(mapBuf);
+	try {
+
+		ifstream file(path);
+		string line, firstword, secondword;
+		stringstream buf;
+		map<string, string> mapBuf;
+
+		while (std::getline(file, line))
+		{
+			buf.str(line);
+			buf >> firstword >> secondword;
+			mapBuf.insert({ firstword, secondword });
+			unit.push_back(mapBuf);
+		}
+
+		file.close();
+	}
+	catch (exception e) {
+		assert(e.what());
 	}
 
-	file.close();
+
 	return unit;
 }
 
+// Handle main menu input
 void menuGetKey(Menu& m)
 {
 	while (!GetAsyncKeyState(VK_ESCAPE))				// Run aslong we didnt push 'ESC'
@@ -127,10 +178,16 @@ void menuGetKey(Menu& m)
 		if (GetAsyncKeyState(VK_RETURN))
 		{
 			system("cls");
-			if (m.getActualPos() == 1)
+			if (m.getActualPos() == 1) {
+				Sleep(500);
 				read();
-			else if (m.getActualPos() == 2)
+				break;
+			}
+			else if (m.getActualPos() == 2) {
+				Sleep(500);
 				write();
+				break;
+			}
 		}
 		Sleep(90);
 	}
@@ -138,6 +195,7 @@ void menuGetKey(Menu& m)
 	system("cls");
 }
 
+// Handle unit switch menu input
 void unitGetKey(Menu& m, vector<map<string, string>>& unit)
 {
 	while (!GetAsyncKeyState(VK_ESCAPE))				// Run aslong we didnt push 'ESC'
@@ -154,9 +212,8 @@ void unitGetKey(Menu& m, vector<map<string, string>>& unit)
 		}
 		if (GetAsyncKeyState(VK_RETURN)) {
 			unit = loadUnit(m.getActualPos());
-			break;
+			return;
 		}
 		Sleep(90);
 	}
-	system("cls");
 }
